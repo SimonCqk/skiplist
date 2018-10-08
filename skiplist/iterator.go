9fb1b04066f5aa87949f791a6962ab5252ef7a9d
Cqk <cqk0100@gmail.com>
2018-10-08 14:05:50 +0800

    fix `next` filed of skip list node.

--- skiplist/iterator.go
+++ skiplist/iterator.go
@@ -15,24 +15,30 @@ func (i *skipListIterator) Key() interface{} {
 
 func (i *skipListIterator) Next() {
 	checkValid(i)
-	i.node = i.node.Next()
+	i.node = i.node.Next(0)
 }
 
 func (i *skipListIterator) Prev() {
 	checkValid(i)
-
+	i.node = i.list.findLessThan(i.node.Key)
+	if i.node == i.list.head {
+		i.node = nil
+	}
 }
 
-func (i *skipListIterator) Seek() {
-
+func (i *skipListIterator) Seek(target interface{}) {
+	i.node = i.list.findGreaterOrEqual(target, nil)
 }
 
 func (i *skipListIterator) SeekToFirst() {
-
+	i.node = i.list.head.Next(0)
 }
 
 func (i *skipListIterator) SeekToLast() {
-
+	i.node = i.list.findLast()
+	if i.node == i.list.head {
+		i.node = nil
+	}
 }
 
 func checkValid(iter *skipListIterator) {
